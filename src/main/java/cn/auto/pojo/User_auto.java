48602cbaba1b2715e782f325d400d81db489744c
alucard <1328834833@qq.com>
2019-05-05 09:25:20 +0800

    更新接口和后台界面

--- src/main/java/cn/auto/pojo/User_auto.java
+++ src/main/java/cn/auto/pojo/User_auto.java
@@ -5,10 +5,45 @@ public class User_auto {
     private Integer Id;         //汽车编号
     private String autoName;    //汽车名称
     private String production;  //汽车出厂日期
-    private Integer autoTypeId; //汽车品牌
+    private Auto_type autoTypeId; //汽车品牌
+    private Configuration autoConfig; //汽车配置
     private String autoColor;   //汽车颜色
     private String autoMoney;   //汽车价格
     private String autoImg;     //汽车图片路径
+    private Integer adminAuto; //商店所有权
+    private Integer userAuto;   //用户所有权
+
+    public Auto_type getAutoTypeId() {
+        return autoTypeId;
+    }
+
+    public void setAutoTypeId(Auto_type autoTypeId) {
+        this.autoTypeId = autoTypeId;
+    }
+
+    public Configuration getAutoConfig() {
+        return autoConfig;
+    }
+
+    public void setAutoConfig(Configuration autoConfig) {
+        this.autoConfig = autoConfig;
+    }
+
+    public Integer getAdminAuto() {
+        return adminAuto;
+    }
+
+    public void setAdminAuto(Integer adminAuto) {
+        this.adminAuto = adminAuto;
+    }
+
+    public Integer getUserAuto() {
+        return userAuto;
+    }
+
+    public void setUserAuto(Integer userAuto) {
+        this.userAuto = userAuto;
+    }
 
     public Integer getId() {
         return Id;
@@ -34,13 +69,6 @@ public class User_auto {
         this.production = production;
     }
 
-    public Integer getAutoTypeId() {
-        return autoTypeId;
-    }
-
-    public void setAutoTypeId(Integer autoTypeId) {
-        this.autoTypeId = autoTypeId;
-    }
 
     public String getAutoColor() {
         return autoColor;
